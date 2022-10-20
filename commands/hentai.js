c23e41e0009252d1306f7fa8ec837a793ae0964d
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-20 10:10:11 +0000

    fixed command usage logging in hentai cmd

--- commands/hentai.js
+++ commands/hentai.js
@@ -7,13 +7,13 @@ module.exports = {
     .setName("hentai")
     .setDescription("Sends a hentai photo"),
   async execute(interaction) {
+    console.log(`User ${interaction.user.username} has used the hentai command in "${interaction.guild.name}"`)
     if (interaction.channel.nsfw) {
             return interaction.reply(await hmtai.nsfw.hentai());
         } else 
       return interaction.reply({
         content: `this command only works on nsfw channels`,
         ephemeral: true,
-      })
-       console.log(`User ${interaction.user.username} has used the hentai command in "${interaction.guild.name}"`)
+      }); 
   },
 };
