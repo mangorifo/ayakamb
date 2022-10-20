72f1b5f85a3878b1ffcba25e3a143491a30fe9c0
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-20 14:15:03 +0800

    update help command

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('help')
         .setDescription('help!'),
     async execute(interaction) {
-    return interaction.reply(`**help** for Ayaka-MB (hb, soon :skull:)\nhentai: sends hentai :skull:\nhowgay: how gay are you?\nping: sends bot's ping\navatar: sends URL of a member's avatar (or yours)\nsus: sends a sus gif\nrandip: random ip address\ndeadchat: dead chat gif\n\n\nYes, sadly the music commands are gone`);
+    return interaction.reply(`**help** for Ayaka-MB (hb, soon :skull:)\nhentai: sends hentai :skull:\nhowgay: how gay are you?\nping: sends bot's ping\navatar: sends URL of a member's avatar (or yours)\nsus: sends a sus gif\nrandip: random ip address\ndeadchat: dead chat gif\n**MODERATION**\nban: bans a member\ntimeout: times out a member\nunban: unbans a member\nkick: kicks a member\n\nYes, sadly the music commands are gone`);
     },
 };
