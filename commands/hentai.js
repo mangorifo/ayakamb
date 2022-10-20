1c093f06307a08142dc5dccbc0c1cbb186494c88
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-20 14:35:51 +0800

    Henta

--- commands/hentai.js
+++ commands/hentai.js
@@ -1,13 +1,12 @@
 const { SlashCommandBuilder } = require("discord.js");
-const nHentai = require('shentai')
-const sHentai = new nHentai
+const HMtai = require("hmtai");
+const hmtai = new HMtai();
 const Discord = require('discord.js');
-const doujin = await sHentai.getRandom()
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("hentai")
     .setDescription("Sends a hentai photo"),
   async execute(interaction) {
-    return interaction.reply(doujin);
+    return interaction.reply(await hmtai.nsfw.hentai());
   },
-};
+}
