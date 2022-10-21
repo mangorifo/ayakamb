22b6657dea0560e85b5acf797f3a262011a77a3f
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-21 19:37:37 +0800

    aasdasdasf

--- commands/howgay.js
+++ commands/howgay.js
@@ -7,16 +7,26 @@ module.exports = {
       option.setName("user").setDescription("if u wanna find out if the selected user is gay").setRequired(false)
     ),
 	async execute(interaction) {
+		const gay = [Math.floor(Math.random() * 101)];
     const user = interaction.options.getMember("user");
+	const notgay = [
+		`1024548642750726144`,
+		`716491639857872928`,
+		`675108484341301275`,
+	]
 		if (!user)
       return interaction.reply({
-        content: "that is not a user",
+        content: `${user} is ${gay}% gay <a:senti:1031904003866509382>`,
         ephemeral: true,
       });
+	  if (!notgay)
+	  return interaction.reply({
+		content: `${user} is ${gay}% gay <a:senti:1031904003866509382>`
+	  })
 		const whois = interaction.options.getString("who");
 		
 		console.log('[LOG] | "howgay.js" was used')
-    const gay = [Math.floor(Math.random() * 101)];
-		return interaction.reply(`${user} is ${gay}% gay <a:senti:1031904003866509382>`);
+    
+		return interaction.reply(`${user} is 0% gay <a:senti:1031904003866509382>`);
 	},
 };
