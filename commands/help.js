0901f9a3c7c914c55872506aeaf86b435ab939ec
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-21 14:41:18 +0800

    update help command to show hu tao voiceline command

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,8 @@ module.exports = {
 	.setColor(`blue`)
 	.setTitle(`All commands`)
 	.setAuthor({ name: 'help command'})
-	.setDescription(`/avatar: sends an avatar url of your avatar or the specified user\n/ayakavoiceline: sends a random voiceline from Ayaka from the game "Genshin Impact"\n/ban: bans a user\n/deadchat: sends a "dead chat" gif\n/help: shows this embed\n/hentai: you know what this command does...\n/howgay: makes the bot guess how gay you are\n/howsus: makes the bot guess how sus you are\n/kick: kicks a user\n/ping: checks the bot latency\n/randomip: sends a random IP address\n/server: sends information about the server\n/sus: sends a "sus" gif\n/timeout: times out a user\n/unban: unbans a user\n/user: sends info about you`)
+	.setDescription(`Fun, utilities and *sus*/avatar: sends an avatar url of your avatar or the specified user\n/ban: bans a user\n/deadchat: sends a "dead chat" gif\n/help: shows this embed\n/hentai: you know what this command does...\n/howgay: makes the bot guess how gay you are\n/howsus: makes the bot guess how sus you are\n/kick: kicks a user\n/ping: checks the bot latency\n/randomip: sends a random IP address\n/server: sends information about the server\n/sus: sends a "sus" gif\n/timeout: times out a user\n/unban: unbans a user\n/user: sends info about you`)
+  .addField(`Others\n\n/ayakavoiceline: sends a random voiceline of Ayaka from the game "Genshin Impact"\n/hutaovoiceline: sends a random voiceline of Hu Tao from "Genshin Impact"`)
 		return interaction.reply({ embeds: [messageEmbed] });
     },
 };
