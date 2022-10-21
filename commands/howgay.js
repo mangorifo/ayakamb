0e4bdc021594ace0494b338e5a1263fe11e14137
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-21 19:20:25 +0800

    yes

--- commands/howgay.js
+++ commands/howgay.js
@@ -2,8 +2,18 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('howgay')
-		.setDescription('how gay are you???'),
+		.setDescription('how gay are you???')
+		.addUserOption((option) =>
+      option.setName("user").setDescription("if u wanna find out if the mentioned user is gay").setRequired(false)
+    ),
 	async execute(interaction) {
+		if (!user)
+      return interaction.reply({
+        content: "that is not a user",
+        ephemeral: true,
+      });
+		const whois = interaction.options.getString("who");
+		
 		console.log('[LOG] | "howgay.js" was used')
     const gay = [Math.floor(Math.random() * 101)];
 		return interaction.reply(`you are: ${gay}% gay <a:senti:1031904003866509382>`);
