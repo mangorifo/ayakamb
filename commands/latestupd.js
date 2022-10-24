548e2eab5bb2c6b4a7c9e002af4cbeed86c59d02
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-24 05:11:41 +0000

    update changelog

--- commands/latestupd.js
+++ commands/latestupd.js
@@ -9,7 +9,7 @@ module.exports = {
 	.setColor(`00FF00`)
 	.setTitle(`Changelog`)
 	.setAuthor({ name: 'latest update of AyakaMBot'})
-	.setDescription(`*update commands*\n\nupdated the hentai command`)
+	.setDescription(`*update commands*\n\nupdated the hentai command\nupdated embed colors`)
         return interaction.reply({ embeds: [messageEmbed] });
     },
 };
