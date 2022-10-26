c766f4ffb59339d3a891f690cfb645a66ff36f8c
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-26 10:56:13 +0000

    Fixe ²

--- commands/rhelp.js
+++ commands/rhelp.js
@@ -11,7 +11,7 @@ module.exports = {
 			.addChoices(
         { name: 'What is localhost?', value: 'lchostsearch'},
          { name: 'How do I make a discord bot?', value: 'htmdb'},
-        { name: 'What is HTTP?', value: 'httpm'},
+        { name: 'What is HTTP?', value: 'http'},
 			)),
     async execute(interaction) {
      const src = interaction.options.getString('search');
@@ -21,7 +21,7 @@ module.exports = {
 	.setTitle(`Localhost`)
   .setURL('https://whatismyipaddress.com/ip/127.0.0.1')
 	.setAuthor({ name: 'rhelp'})
-	.setDescription(`In a computer network, localhost is a hostname that refers to the computer that is executing a program — you can think of it as meaning “this computer.” The term is used when making a loopback request to one's own device.\n\nThe localhost is the default name describing the local computer address also known as the loopback address. For example, typing: ping localhost would ping the local IP address of 127.0. 0.1 (the loopback address).`)
+	.setDescription(`In a computer network, localhost is a hostname that refers to the computer that is executing a program — you can think of it as meaning “this computer.” The term is used when making a loopback request to one's own device.\n\nThe localhost is the default name describing the local computer address also known as the loopback address. For example, typing: ping localhost would ping the local IP address of 127.0.0.1 (the loopback address).`)
 
       const htmdbEmbed = new EmbedBuilder()
 	.setColor(`e4b400`)
@@ -29,7 +29,7 @@ module.exports = {
   .setURL('https://discordjs.guide')
 	.setAuthor({ name: 'rhelp'})
 	.setDescription(`can't explain how to make a discord bot here in one embed. go [here](https://discordjs.guide) for a guide of how.`)
-      const httpmbed = new EmbedBuilder()
+      const httpembed = new EmbedBuilder()
 	.setColor(`e4b400`)
 	.setTitle(`What is HTTP?`)
 .setURL('https://developer.mozilla.org/en-US/docs/Web/HTTP')
@@ -41,10 +41,9 @@ module.exports = {
       else if (interaction.options.getString('search') == 'htmdb') {
         return interaction.reply({ embeds: [htmdbEmbed], ephemeral: false })
       }
-      if (interaction.options.getString('search') == 'httpm') {
-       return interaction.reply({ embeds: [httpmbed], ephemeral: false})
+      if (interaction.options.getString('search') == 'http') {
+       return interaction.reply({ embeds: [httpembed], ephemeral: false});
      }
 
-		return interaction.reply('wat how');
     },
 };
