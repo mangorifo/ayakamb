90cfc044777eca1ac7cbfd193caa7f7935c76a96
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-11-16 13:17:03 +0000

    real

--- commands/httpcat.js
+++ commands/httpcat.js
@@ -1,10 +1,18 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { img } = require('./text/json/httpcat.json');
+const { isc } = require('./text/json/isExistingCode.json')
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('httpcat')
-    .setDescription(`Sends a random HTTP cat photo.`),
+    .setDescription(`Sends a random HTTP cat photo.`)
+    .addStringOption((option) =>
+      option
+        .setName("search")
+        .setDescription("search for an error code")
+        .setRequired(false)
+    ),
   async execute(interaction) {
+  const squery = interaction.options.getString("search");
     const row = new ActionRowBuilder()
       .addComponents(
         new ButtonBuilder()
@@ -12,6 +20,13 @@ module.exports = {
           .setLabel('Send me another one')
           .setStyle(ButtonStyle.Secondary),
       );
+
+    if (isc.includes(squery)) { 
+      return interaction.reply(`https://http.cat/${squery}`);
+    }
+    if (!isc.includes(squery)) { 
+      return interaction.reply(`That error code does not exist. (${squery})`);
+    }
     const catgenerator = img[Math.floor(Math.random() * img.length)];
     const cat1 = new EmbedBuilder()
       .setTitle(`HTTP Cat`)
@@ -19,7 +34,7 @@ module.exports = {
       .setImage(catgenerator)
     console.log('[LOG] | "httpcat.js" was used')
 
-    await interaction.reply({ embeds: [cat1], components: [row] })
+     await interaction.reply({ embeds: [cat1], components: [row] })
     const filter = i => i.customId === 'cat' && i.user.id === interaction.user.id;
 
     const collector = interaction.channel.createMessageComponentCollector({ filter });
